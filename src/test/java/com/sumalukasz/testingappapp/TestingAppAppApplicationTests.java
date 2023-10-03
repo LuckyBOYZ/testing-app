1c3908766f4ee7a633a641a29861e5038faeef9f
Łukasz Suma <lukaszsuma91@gmail.com>
2023-10-03 15:42:18 +0200

    init commit

---
+++ src/test/java/com/sumalukasz/testingappapp/TestingAppAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sumalukasz.testingappapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestingAppAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
